c07ec23c85276d86acea958c77b580136bc0cdac
Andrey <andreycesar02@gmail.com>
2025-04-09 19:02:29 -0300

    Finalizando primeiros passos

--- 4seg/routes/web.php
+++ 4seg/routes/web.php
@@ -2,19 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Controller;
+use App\Http\Controllers\HomeController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::resource('4seg', Controller::class);
-
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified',
-])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
-});
+Route::get('/', [HomeController::class, 'index'])->middleware('auth');
+Route::resource('4seg', HomeController::class)->middleware('auth');
